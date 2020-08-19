229908ecd60352c9b66bd959f3d1c1a99a0c1059
sathish palvai <sathish.palvai@us.abb.com>
2020-08-19 09:13:52 -0700

    applied styling to host element to suit angular

--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
-  @Input() sections;
+  @Input() section;
   constructor() { }
 
   ngOnInit(): void {
